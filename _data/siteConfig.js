a1b46f2278bd7789e6df2fffe99a04edaa3b9dad
nebfield <nebfield@users.noreply.github.com>
2024-11-25 15:44:42 +0000

    deploy: 5ae5707ad2a5722838bb1e992663508eb3122358

--- _data/siteConfig.js
+++ _data/siteConfig.js
@@ -1,14 +1,11 @@
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "INTERVENE platform",
-    short_description: 'We integrate AI and human genetics to develop tools for disease prevention, diagnosis, and personalised treatment.',
+    title: "GeneticScores.org",
+    short_description: "We integrate AI and human genetics to develop tools for disease prevention, diagnosis, and personalised treatment.",
     url: "https://intervenegeneticscores.org/",
     author: "Genetic Data Platform team",
     email: "bwingfield@ebi.ac.uk",
   },
-  heroInformation: {
-    text: "Calculate polygenic scores now",
-    link: "https://google.com"
-  }
+  siteVersion: "0.1"
 };
